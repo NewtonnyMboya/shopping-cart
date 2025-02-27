896a5f837743c830b473d809a5f22a1b444f36cd
Newtonny Wambua <alvatnoz@gmail.com>
2025-02-27 10:20:08 -0500

    development purposes
    for development naturalisation

---
+++ src/com/tonny/service/DemandService.java
@@ -0,0 +1,17 @@
+package com.tonny.service;
+
+import java.util.List;
+
+import com.tonny.beans.DemandBean;
+
+public interface DemandService {
+
+	public boolean addProduct(String userId, String prodId, int demandQty);
+
+	public boolean addProduct(DemandBean userDemandBean);
+
+	public boolean removeProduct(String userId, String prodId);
+
+	public List<DemandBean> haveDemanded(String prodId);
+
+}
